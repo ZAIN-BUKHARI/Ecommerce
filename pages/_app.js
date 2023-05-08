755de29798be3755ec5d89c6bd8b5a3a7ccec5c4
ZAIN-BUKHARI <zainnnnnnnnnnnnnnnnnnnn@gmail.com>
2023-05-08 09:15:36 -0700

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import '@/styles/globals.css'
+import {CartContextProvider} from "@/components/CartContext";
+
+
+
+export default function App({ Component, pageProps }) {
+  return( 
+  <>
+  <CartContextProvider>
+        <Component {...pageProps} />
+      </CartContextProvider>
+  </>
+  )
+}
